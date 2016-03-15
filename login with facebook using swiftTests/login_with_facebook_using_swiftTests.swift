86e4876a34b0bd71adbabf6f955bbad910aab824
macbook pro <macbookpro@macbooks-macbook-pro-2.local>
2016-03-15 13:06:52 -0700

    Initial Commit

---
+++ login with facebook using swiftTests/login_with_facebook_using_swiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  login_with_facebook_using_swiftTests.swift
+//  login with facebook using swiftTests
+//
+//  Created by macbook pro on 3/15/16.
+//  Copyright © 2016 macbook pro. All rights reserved.
+//
+
+import XCTest
+@testable import login_with_facebook_using_swift
+
+class login_with_facebook_using_swiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
